51fc7282565065fcd638bcbf1fa19fd962acfe33
eximmlich@gmail.com <eximmlich@gmail.com>
2023-06-09 11:36:00 +0700

    init

---
+++ SoloChat/Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SoloChat
+//
+//  Created by Даниил Назаров on 09.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		view.backgroundColor = .red
+	}
+
+
+}
+
